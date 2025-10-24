20b7a9a4128e27ba68506a2bd0cd25a454b961ea
Misbah-Engr <bichistriver@aliyu.localdomain>
2025-10-24 07:00:59 +0100

    remove problematic frontend implementation

--- frontend/src/components/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Skeleton: React.FC = () => {
-  return (
-    <div className="animate-pulse bg-gray-700 rounded-md h-4 w-full"></div>
-  );
-};
-
-export default Skeleton;
